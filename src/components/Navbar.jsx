a5a23a13fb9848d00b4905e9e07fd1506f85b730
ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>
2025-01-26 03:52:29 +0530

    testimonials/done
    Signed-off-by: ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,84 @@
+import clsx from "clsx";
+import gsap from "gsap";
+import { useWindowScroll } from "react-use";
+import { useEffect, useRef, useState } from "react";
+import { TiLocationArrow } from "react-icons/ti";
+
+import Button from "./Button";
+
+const navItems = ["Prizes", "Timeline", "FAQs", "Testimonials", "Contact"];
+
+const NavBar = () => {
+  // Refs for navigation container
+  const navContainerRef = useRef(null);
+
+  const { y: currentScrollY } = useWindowScroll();
+  const [isNavVisible, setIsNavVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    if (currentScrollY === 0) {
+      // Topmost position: show navbar without floating-nav
+      setIsNavVisible(true);
+      navContainerRef.current.classList.remove("floating-nav");
+    } else if (currentScrollY > lastScrollY) {
+      // Scrolling down: hide navbar and apply floating-nav
+      setIsNavVisible(false);
+      navContainerRef.current.classList.add("floating-nav");
+    } else if (currentScrollY < lastScrollY) {
+      // Scrolling up: show navbar with floating-nav
+      setIsNavVisible(true);
+      navContainerRef.current.classList.add("floating-nav");
+    }
+
+    setLastScrollY(currentScrollY);
+  }, [currentScrollY, lastScrollY]);
+
+  useEffect(() => {
+    gsap.to(navContainerRef.current, {
+      y: isNavVisible ? 0 : -100,
+      opacity: isNavVisible ? 1 : 0,
+      duration: 0.2,
+    });
+  }, [isNavVisible]);
+
+  return (
+    <div
+      ref={navContainerRef}
+      className="fixed w-[94%] md:w-[98%] top-4 z-50 py-4 md:px-9 px-4 border-none transition-all duration-700 rounded-lg "
+    >
+      <header className="w-full ">
+        <nav className="flex items-center justify-between">
+          {/* Logo and Product button */}
+          <div className="flex items-center gap-7">
+            <img src="/img/logo.svg" alt="logo" />
+
+            <Button
+              id="product-button"
+              title="Brochure"
+              rightIcon={<TiLocationArrow />}
+              containerClass="bg-blue-50 flex items-center justify-center gap-1"
+            />
+          </div>
+
+          {/* Navigation Links and Audio Button */}
+          <div className="flex h-full items-center">
+            <div className="hidden md:block">
+              {navItems.map((item, index) => (
+                <a
+                  key={index}
+                  href={`#${item.toLowerCase()}`}
+                  className="nav-hover-btn"
+                >
+                  {item}
+                </a>
+              ))}
+            </div>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default NavBar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Hero from '../sections/Hero'
+import Testimonials from '../sections/Testimonials'
 
 const Home = () => {
   return (
-    <div>
-      home
+    <div className='flex flex-col gap-[150px]'>
+      <Hero />
+      <Testimonials />
     </div>
   )
 }
