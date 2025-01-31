d3d09e6fffa98c0353e3798bd731699af4aa1b06
ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>
2025-01-31 06:56:27 +0530

    CountUp/added
    Signed-off-by: ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>

--- src/components/StatCard.jsx
+++ src/components/StatCard.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import CountUp from "./CountUp";
 
-const StatCard = ({data, tag}) => {
+const StatCard = ({ data, tag }) => {
   return (
     <div className="flex font-['shakuro']  flex-col gap-0 text-white">
-      <p className="text-8xl md:text-8xl">{data}</p>
+      <div className="flex items-center">
+        <p className="text-8xl md:text-8xl">
+          <CountUp
+            from={0}
+            to={data}
+            separator=","
+            direction="up"
+            duration={2}
+          />
+        </p>
+        <p className="text-8xl md:text-8xl">+</p>
+      </div>
+
       <p className="text-2xl text-white">{tag}</p>
     </div>
   );
