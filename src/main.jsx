151de285467ff1cd288366b5448164abdc740f61
Mitesh2806 <miteshmaity_22519@aitpune.edu.in>
2025-01-31 06:42:34 +0530

    Hello

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import App from "./App";
 import "./index.css";
 import Home from './pages/Home'
 
+
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App/>}>
@@ -15,6 +17,7 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    
     <RouterProvider router={router}/>
   </React.StrictMode>
 );
