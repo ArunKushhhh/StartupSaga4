4fbd0cf68293faa84a48b59b97e2427320fc0724
ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>
2025-02-17 20:34:34 +0530

    cursor
    Signed-off-by: ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,26 @@ import React from "react";
 import AnimatedCursor from "react-animated-cursor";
 import { Outlet } from "react-router-dom";
 
-
 const App = () => {
   return (
     <main>
-     <AnimatedCursor
-      innerSize={8}
-      outerSize={35}
-      innerScale={1}
-      outerScale={2}
-      outerAlpha={0}
-      hasBlendMode={true}
-      innerStyle={{
-        backgroundColor: "white"
-      }}
-      outerStyle={{
-        border: '1px solid white'
-      }}
-      
-     />
+      <div className="hidden lg:block">
+        <AnimatedCursor
+          innerSize={8}
+          outerSize={35}
+          innerScale={1}
+          outerScale={2}
+          outerAlpha={0}
+          hasBlendMode={true}
+          innerStyle={{
+            backgroundColor: "white",
+          }}
+          outerStyle={{
+            border: "1px solid white",
+          }}
+        />
+      </div>
       <Outlet />
-    
     </main>
   );
 };
