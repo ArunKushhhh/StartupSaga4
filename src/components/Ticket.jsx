a1885429d091082a0c4d580e92587dcf2211f139
ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>
2025-02-20 08:33:56 +0530

    packages/fixed
    Signed-off-by: ArunKushhhh <arunkushwaha_230604@aitpune.edu.in>

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -4,7 +4,7 @@ import Form from "./Form";
 
 const Ticket = () => {
   return (
-    <div className="flex flex-col md:flex-row w-full md:min-h-[60vh] gap-0 px-4 md:px-16">
+    <div className="flex flex-col md:flex-row w-full md:min-h-[60vh] gap-0 px-4 md:px-16 text-black">
       <div className="relative w-full min-h-24 md:w-[10%] bg-[#FFFBF5] border-none outline-none rounded-t-3xl md:rounded-l-3xl overflow-hidden flex justify-center items-center">
         <div className="absolute left-0 md:left-[100%] bottom-0 md:top-0 w-14 h-14 rounded-full bg-[#001223] -translate-x-1/2 translate-y-1/2 md:-translate-y-1/2" />
         <div className="rotate-0 md:rotate-90 uppercase text-[0.9em] md:text-[1.2em] text-center tracking-[1rem]">
